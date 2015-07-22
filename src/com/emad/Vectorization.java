3fa5a1d07a34737b4f24906b58b4250f6b08765d
Emad <emad.khezri@gmail.com>
2015-07-22 18:45:13 +0430

    - code convention have been set

--- src/com/emad/Vectorization.java
+++ src/com/emad/Vectorization.java
@@ -3,19 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.emad;
 
 /**
  *
  * @author tnp
  */
-public class Vectorization {
+public class Vectorization
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+
     }
-    
+
 }
